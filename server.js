8cc9a6d0628b9d5bd4166084118bb05cb1d79cf0
Herve <herveadoufouli4@gmail.com>
2026-06-13 14:44:05 +0100

    Fix port Railway

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
